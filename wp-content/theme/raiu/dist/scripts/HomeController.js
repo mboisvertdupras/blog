5914579e1be3f995f1a23026678a2ac808988bc4
Marc Boisvert <bladetecltd@gmail.com>
2019-11-29 00:49:52 -0500

    deploy: CREATE wp-content/theme/raiu/dist/scripts/HomeController.js

---
+++ wp-content/theme/raiu/dist/scripts/HomeController.js
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en-US">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+  <link href="https://fonts.googleapis.com/css?family=DM+Sans:400,700|Red+Hat+Display:900|Prompt:400,700" rel="stylesheet">
+  <title>boisvertdupras | Just another WordPress site</title>
+
+<!-- The SEO Framework by Sybre Waaijer -->
+<meta name="robots" content="max-snippet:-1,max-image-preview:standard,max-video-preview:-1" />
+<meta name="description" content="Just another WordPress site on boisvertdupras&#8230;" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="boisvertdupras | Just another WordPress site" />
+<meta property="og:description" content="Just another WordPress site on boisvertdupras&#8230;" />
+<meta property="og:url" content="https://boisvertdupras.com/" />
+<meta property="og:site_name" content="boisvertdupras" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="boisvertdupras | Just another WordPress site" />
+<meta name="twitter:description" content="Just another WordPress site on boisvertdupras&#8230;" />
+<link rel="canonical" href="https://boisvertdupras.com/" />
+<script type="application/ld+json">{"@context":"https://schema.org","@type":"WebSite","url":"https://boisvertdupras.com/","name":"boisvertdupras","potentialAction":{"@type":"SearchAction","target":"https://boisvertdupras.com/search/{search_term_string}","query-input":"required name=search_term_string"}}</script>
+<script type="application/ld+json">{"@context":"https://schema.org","@type":"Organization","url":"https://boisvertdupras.com/","name":"boisvertdupras"}</script>
+<!-- / The SEO Framework by Sybre Waaijer | 0.27ms meta | 0.29ms boot -->
+
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='wp-block-library-css'  href='https://boisvertdupras.com/wp-includes/css/dist/block-library/style.min.css?ver=5.4-alpha-46797' type='text/css' media='all' />
+<link rel='stylesheet' id='app-css-css'  href='https://boisvertdupras.com/wp-content/themes/raiu/dist/styles/app.css' type='text/css' media='all' />
+<script type='text/javascript' src='https://boisvertdupras.com/wp-content/themes/raiu/dist/scripts/manifest.js'></script>
+<link rel='https://api.w.org/' href='https://boisvertdupras.com/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://boisvertdupras.com/xmlrpc.php?rsd" />
+<link rel="alternate" type="application/json+oembed" href="https://boisvertdupras.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fboisvertdupras.test%2F" />
+<link rel="alternate" type="text/xml+oembed" href="https://boisvertdupras.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fboisvertdupras.test%2F&#038;format=xml" />
+<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style></head>
+  <body class="home page-template-default page page-id-5 app-data index-data singular-data page-data page-5-data page-home-data front-page-data">
+        <div class="wrap" role="document">
+      <div class="content">
+        <main class="main">
+          <div class="flex flex-column hero">
+    <header id="navbar" class="navbar">
+    <a class="brand" href="https://boisvertdupras.com/">
+      
+      <svg viewBox="0 0 217 295" fill="none" xmlns="http://www.w3.org/2000/svg"><path id="logopath" d="M15 163.5v116h93.375M15 163.5h93.375M15 163.5v-136l93.375 57 93.375-57v136m-93.375 116v-57m0 57h93.375v-116m-93.375 59H15m93.375 0v-59m0 0h93.375" stroke="#000" stroke-width="30"/></svg>
+    </a>
+    <nav class="nav-links">
+              <div class="menu-primary-container"><ul id="menu-primary" class="nav"><li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="https://boisvertdupras.com/blog">Blog</a></li>
+<li id="menu-item-33" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-33"><a href="https://boisvertdupras.com/services">Services</a></li>
+<li id="menu-item-38" class="menu-item menu-item-type-post_type_archive menu-item-object-site menu-item-38"><a href="https://boisvertdupras.com/site">Projects</a></li>
+<li id="menu-item-36" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-36"><a href="https://boisvertdupras.com/contact">Contact</a></li>
+</ul></div>
+          </nav>
+  </header>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="hero-content">
+              <div class="lead">
+                  Hey, my name is <a href="/about">Marc</a>
+              </div>
+              <div class="title-wrapper">
+                  <div class="title clone">
+                      I design and develop websites
+                  </div>
+                  <div class="title clone">
+                      I design and develop websites
+                  </div>
+                  <div class="title clone">
+                      I design and develop websites
+                  </div>
+                  <h1 class="title">
+                      I design and develop websites
+                  </h1>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
+
+        </main>
+      </div>
+    </div>
+        <footer class="content-info">
+  <div class="container">
+      </div>
+</footer>
+    <script type='text/javascript' src='https://boisvertdupras.com/wp-content/themes/raiu/dist/scripts/vendor.js'></script>
+<script type='text/javascript' data-turbolinks-suppress-warning src='https://boisvertdupras.com/wp-content/themes/raiu/dist/scripts/app.js'></script>
+  </body>
+</html>
